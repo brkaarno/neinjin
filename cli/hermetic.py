ba02354325053f3a17d757db538370130cb1083d
brkaarno <128406999+brkaarno@users.noreply.github.com>
2025-04-25 05:25:05 -0400

    Provision 10j build-deps, LLVM, CMake, and OCaml (#2)

--- cli/hermetic.py
+++ cli/hermetic.py
@@ -1,5 +1,7 @@
 import subprocess
+import shutil
 from pathlib import Path
+import os
 
 import click
 
@@ -12,51 +14,89 @@ def check_call_uv(args: list[str]):
     subprocess.check_call([localdir / "uv", "--config-file", localdir / "uv.toml", *args])
 
 
+def xj_build_deps(localdir: Path) -> Path:
+    return localdir / "xj-build-deps"
+
+
+def xj_llvm_root(localdir: Path) -> Path:
+    return localdir / "xj-llvm"
+
+
 def opamroot(localdir: Path) -> Path:
     return localdir / "opamroot"
 
 
-def run_opam(args: list[str], eval_env=True, check=False, **kwargs) -> subprocess.CompletedProcess:
+def opam_non_hermetic() -> bool:
+    running_in_ci = "GITHUB_WORKSPACE" in os.environ
+    return running_in_ci and shutil.which("opam") is not None
+
+
+def run_opam(
+    args: list[str], eval_opam_env=True, with_tenjin_deps=True, check=False, env_ext=None, **kwargs
+) -> subprocess.CompletedProcess:
     localdir = repo_root.localdir()
     localopam = localdir / "opam"
-    if not eval_env:
-        # When running command like opam init, there is no env to source!
-        return subprocess.run(
-            [localopam, *args, "--cli=2.3", "--root", opamroot(localdir)],
-            check=check,
-            shell=False,
-            **kwargs,
-        )
-    else:
-        # Once the root is set up, most subcommands need to run with a suite of
-        # env vars configured by $(opam env).
-        opam_env_cmd = (
-            f"{localopam} env  --cli=2.3 --root {opamroot(localdir)} "
-            + "--switch=tenjin --set-switch --set-root"
-        )
-        maincmd = " ".join(
-            str(x)
-            for x in [
-                localopam,
-                *args,
-                "--cli=2.3",
-                "--switch",
-                "tenjin",
-                "--root",
-                opamroot(localdir),
-            ]
-        )
 
-        return subprocess.run(
-            f"eval $({opam_env_cmd}) && {maincmd}",
-            check=check,
-            shell=True,
-            **kwargs,
-        )
+    def mk_shell_cmd():
+        def shell_cmd(parts: list[str]) -> str:
+            return " ".join(str(x) for x in parts)
+
+        non_hermetic = opam_non_hermetic()
+        if non_hermetic:
+            # We can save about four minutes per run in CI by using the system opam.
+            # If it appears to be installed, use it.
+            opam_root_args = []
+        else:
+            opam_root_args = ["--root", opamroot(localdir)]
+
+        maincmd = shell_cmd([localopam, *args, "--cli=2.3", *opam_root_args])
+
+        if eval_opam_env:
+            if non_hermetic:
+                # If we're using the system opam, we use the default switch.
+                assert shell_cmd(opam_root_args) == ""
+                opam_env_cmd = f"{localopam} env --cli=2.3"
+            else:
+                opam_env_cmd = (
+                    f"{localopam} env --cli=2.3 {shell_cmd(opam_root_args)} "
+                    + "--switch=tenjin --set-switch --set-root"
+                )
+            return f"eval $({opam_env_cmd}) && {maincmd}"
+        else:
+            return maincmd
+
+    def mk_env():
+        if "env" in kwargs:
+            env = kwargs["env"]
+            del kwargs["env"]  # we'll pass it explicitly, so not via kwargs
+        else:
+            env = os.environ.copy()
+
+        if env_ext is not None:
+            env = {**env, **env_ext}
+
+        if with_tenjin_deps:
+            env["PATH"] = os.pathsep.join([
+                str(xj_build_deps(localdir) / "bin"),
+                str(xj_llvm_root(localdir) / "bin"),
+                env["PATH"],
+            ])
+
+        return env
+
+    return subprocess.run(
+        mk_shell_cmd(),
+        check=check,
+        shell=True,
+        env=mk_env(),
+        **kwargs,
+    )
 
 
-def check_call_opam(args: list[str], eval_env=True, **kwargs) -> subprocess.CompletedProcess:
-    cp = run_opam(args, eval_env, check=False, **kwargs)
+def check_call_opam(
+    args: list[str], eval_opam_env=True, with_tenjin_deps=True, **kwargs
+) -> subprocess.CompletedProcess:
+    cp = run_opam(args, eval_opam_env, with_tenjin_deps, check=False, **kwargs)
     if cp.stderr:
         click.echo(cp.stderr, err=True)
     cp.check_returncode()
